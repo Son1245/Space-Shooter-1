6dabda4068450c55c3b317cfb3c421a53cab91c6
Son1245 <hoangson1122005zxc@gmail.com>
2026-01-20 23:56:14 +0700

    demo

---
+++ Assets/Space Shooter Template FREE/Scripts/ShowLog.cs
@@ -0,0 +1,22 @@
+public class ShowLog : MonoBehaviour
+{
+// Start is called before the first frame update
+void Start()
+{
+Debug.Log("Hello World!");
+}
+}
+public class ShowLog : MonoBehaviour
+{
+// Start is called before the first frame update
+void Start()
+{
+Debug.Log("Hello World!");
+}
+
+// Update is called once per frame
+void Update()
+{
+Debug.Log("Update called! " + Time.frameCount);
+}
+}
